2f941870fd28b383ced6f70e78b101558ed70eb9
Dairan <dairan@dairan.com>
2021-09-28 11:55:21 -0300

    refatorada principal view

--- TrackingBRApp/Cenas/Principal/PrincipalView.swift
+++ TrackingBRApp/Cenas/Principal/PrincipalView.swift
@@ -5,48 +5,134 @@
 //  Created by Dairan on 22/08/21.
 //
 
+import CoreData
 import UIKit
+
+// MARK: - PrincipalView
 
 class PrincipalView: UIView {
   // MARK: Lifecycle
 
-  init(viewModel: PrincipalViewModel) {
+  init() {
     super.init(frame: .zero)
-    configurarUI()
-    constraints()
+    addSubview(listagemTableView)
+    configurarConstraits()
+
+    do {
+      try fetchResultController.performFetch()
+    } catch {
+      print("==44===:  error", error)
+    }
   }
 
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
 
-  private var viewModel: PrincipalViewModel?
-  // MARK: Internal
-
-  func configurar(com viewModel: PrincipalViewModel) {
-    self.viewModel = viewModel
-  }
-
   // MARK: Private
 
-  private lazy var label: DTFLabel = {
-    let label = DTFLabel()
-    return label
+  private lazy var fetchResultController: NSFetchedResultsController<Encomenda> = {
+    let fetchRequest: NSFetchRequest<Encomenda> = Encomenda.fetchRequest()
+
+    let ordenador = NSSortDescriptor(key: #keyPath(Encomenda.adicionadoEm), ascending: false)
+    fetchRequest.sortDescriptors = [ordenador]
+
+    let nsfrc = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                           managedObjectContext: GerenciadorCoreData.shared.contexto,
+                                           sectionNameKeyPath: nil,
+                                           cacheName: nil)
+    nsfrc.delegate = self
+
+    return nsfrc
+
   }()
 
-  private func configurar(viewModel: PrincipalViewModel) {
-    self.viewModel = viewModel
+  private lazy var listagemTableView: UITableView = {
+    let tableView = UITableView()
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    tableView.accessibilityIdentifier = "listagem-tableView"
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CellId")
+    tableView.delegate = self
+    tableView.dataSource = self
+    return tableView
+  }()
+}
+
+// MARK: - PrincipalView
+
+extension PrincipalView: UITableViewDataSource, UITableViewDelegate {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    fetchResultController.sections?.count ?? 0
   }
 
-  private func configurarUI() {
-    backgroundColor = .systemYellow
-    addSubview(label)
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    guard let info = fetchResultController.sections?[section] else { return 0 }
+    return info.numberOfObjects
   }
 
-  private func constraints() {
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "CellId", for: indexPath)
+    let encomenda = fetchResultController.object(at: indexPath)
+    cell.textLabel?.text = encomenda.codigo
+    cell.detailTextLabel?.text = encomenda.descricao
+    return cell
+  }
+
+  private func configure() {
+  }
+
+  private func configurarConstraits() {
     NSLayoutConstraint.activate([
-      label.centerXAnchor.constraint(equalTo: centerXAnchor),
-      label.centerYAnchor.constraint(equalTo: centerYAnchor),
+      listagemTableView.topAnchor.constraint(equalTo: topAnchor),
+      listagemTableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+      listagemTableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+      listagemTableView.bottomAnchor.constraint(equalTo: bottomAnchor),
     ])
+  }
+
+  func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+
+
+    let apagarAction = UIContextualAction(style: .destructive, title: "Apagar") { acao, view, aoTerminar in
+      let encomenda = self.fetchResultController.object(at: indexPath)
+      GerenciadorCoreData.shared.contexto.delete(encomenda)
+    }
+
+
+    let config = UISwipeActionsConfiguration(actions: [apagarAction])
+    return config
+  }
+}
+
+// MARK: - PrincipalView
+
+extension PrincipalView: NSFetchedResultsControllerDelegate {
+  func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+    listagemTableView.beginUpdates()
+  }
+
+  func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+    switch type {
+    case .insert:
+      if let newIndexPath = newIndexPath {
+        listagemTableView.insertRows(at: [newIndexPath], with: .automatic)
+      }
+    case .delete:
+      if let indexPath = indexPath {
+        listagemTableView.deleteRows(at: [indexPath], with: .automatic)
+      }
+    case .move:
+      break
+    case .update:
+      if let indexPath = indexPath {
+        listagemTableView.reloadRows(at: [indexPath], with: .automatic)
+      }
+    @unknown default:
+      break
+    }
+  }
+
+  func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+    listagemTableView.endUpdates()
   }
 }
--- TrackingBRApp/Cenas/Principal/PrincipalViewController.swift
+++ TrackingBRApp/Cenas/Principal/PrincipalViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Dairan on 22/08/21.
 //
 
-import CoreData
+// import CoreData
 import UIKit
 
 // MARK: - PrincipalViewController
@@ -24,22 +24,9 @@ class PrincipalViewController: UIViewController {
 
   // MARK: Internal
 
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(true)
-
-    do {
-      try fetchResultController.performFetch()
-    } catch {
-      print("==44===:  error", error)
-    }
-  }
-
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.addSubview(listagemTableView)
     configurarNavBar()
-    configurarConstraits()
-//    bind(viewModel)
   }
 
   override func loadView() {
@@ -48,37 +35,10 @@ class PrincipalViewController: UIViewController {
 
   // MARK: Private
 
-  private lazy var fetchResultController: NSFetchedResultsController<Encomenda> = {
-    let fetchRequest: NSFetchRequest<Encomenda> = Encomenda.fetchRequest()
-
-    let ordenador = NSSortDescriptor(key: #keyPath(Encomenda.adicionadoEm), ascending: false)
-    fetchRequest.sortDescriptors = [ordenador]
-
-    let nsfrc = NSFetchedResultsController(fetchRequest: fetchRequest,
-                                           managedObjectContext: GerenciadorCoreData.shared.contexto,
-                                           sectionNameKeyPath: nil,
-                                           cacheName: nil)
-    nsfrc.delegate = self
-
-    return nsfrc
-
-  }()
-
-  private lazy var listagemTableView: UITableView = {
-    let tableView = UITableView()
-    tableView.translatesAutoresizingMaskIntoConstraints = false
-    tableView.accessibilityIdentifier = "listagem-tableView"
-    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CellId")
-    tableView.delegate = self
-    tableView.dataSource = self
-    return tableView
-  }()
-
   private let viewModel: PrincipalViewModel
-//  private let repositorio: Repositorio
 
   private lazy var principalView: PrincipalView = {
-    let view = PrincipalView(viewModel: self.viewModel)
+    let view = PrincipalView()
     return view
   }()
 
@@ -90,13 +50,15 @@ class PrincipalViewController: UIViewController {
     navigationItem.title = "Listagem"
 
     let icone = UIImage(systemName: "gearshape.fill")!
-    let configuracoesButtonItem = UIBarButtonItem(image: icone, style: .done, target: self, action: #selector(configuracoesTapped))
+    let configuracoesButtonItem = UIBarButtonItem(image: icone,
+                                                  style: .done,
+                                                  target: self,
+                                                  action: #selector(configuracoesTapped))
     navigationItem.leftBarButtonItem = configuracoesButtonItem
   }
 
   @objc private func adicionarTapped() {
     let vc = AdicionarEditarViewController()
-//    vc.delegate = self
     vc.title = "Adicionar/ Editar"
 
     let nav = UINavigationController(rootViewController: vc)
@@ -109,89 +71,7 @@ class PrincipalViewController: UIViewController {
 
   @objc private func configuracoesTapped() {
     let vc = ConfiguracoesViewController()
-    show(vc, sender: nil)
+    showDetailViewController(vc, sender: nil)
   }
 
-//  private func bind(_ viewModel: PrincipalViewModel) {
-//    viewModel.atualizarView = {
-//      DispatchQueue.main.async {
-//        self.principalView.configurar(com: viewModel)
-//      }
-//    }
-//  }
-
-  private func configurarConstraits() {
-    NSLayoutConstraint.activate([
-      listagemTableView.topAnchor.constraint(equalTo: view.topAnchor),
-      listagemTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-      listagemTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-      listagemTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-    ])
-  }
-}
-
-// MARK: - PrincipalViewController
-
-// extension PrincipalViewController: AdicionarEditarDelegate {
-//  func salvarTappedTexto(texto: String) {
-//    print(texto)
-//    let viewModel = PrincipalViewModel()
-//    viewModel.titulo = texto
-//    self.principalView.configurar(com: viewModel)
-//  }
-// }
-
-extension PrincipalViewController: UITableViewDataSource, UITableViewDelegate {
-  func numberOfSections(in tableView: UITableView) -> Int {
-    fetchResultController.sections?.count ?? 0
-  }
-
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    guard let info = fetchResultController.sections?[section] else { return 0 }
-    return info.numberOfObjects
-  }
-
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "CellId", for: indexPath)
-    let encomenda = fetchResultController.object(at: indexPath)
-    cell.textLabel?.text = encomenda.codigo
-    cell.detailTextLabel?.text = encomenda.descricao
-    return cell
-  }
-
-  private func configure() {
-  }
-}
-
-// MARK: - PrincipalViewController
-
-extension PrincipalViewController: NSFetchedResultsControllerDelegate {
-  func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-    listagemTableView.beginUpdates()
-  }
-
-  func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-    switch type {
-    case .insert:
-      if let newIndexPath = newIndexPath {
-        listagemTableView.insertRows(at: [newIndexPath], with: .automatic)
-      }
-    case .delete:
-      if let indexPath = indexPath {
-        listagemTableView.deleteRows(at: [indexPath], with: .automatic)
-      }
-    case .move:
-      break
-    case .update:
-      if let indexPath = indexPath {
-        listagemTableView.reloadRows(at: [indexPath], with: .automatic)
-      }
-      @unknown default:
-        break
-    }
-  }
-
-  func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-    listagemTableView.endUpdates()
-  }
 }
